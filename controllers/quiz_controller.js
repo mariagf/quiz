8944943ac709af80bdfdb8a754ae09a67a704ea5
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-07 19:01:13 +0200

    New version

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,37 +1,33 @@
 var models = require('../models/models.js');
 
-//Autoload - factoriza el código si ruta incluye :quizId
-exports.load = function(req, res, next, quizId){
-	models.Quiz.find(quizId).then(
-		function(quiz){
-			if(quiz){
-				req.quiz = quiz;
-				next();
-			} else { next(new Error('No existe quizId=' + quizId));}
-		}). catch(function(error){next(error);});
-};
-
 // GET /quizes
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index',{ quizes: quizes});
-	}).catch(function(error){next(error);})
+		//for (i = 0, i < quiz.length(), i++){
+		  res.render('quizes/index',{ quizes: quizes})
+		  //res.render('quizes/question',{ pregunta: '¿Italia?'})
+		//}
+	})	
 };
 
 // GET /quizes/:id
 exports.show = function(req,res){
-	//models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{ quiz: req.quiz});
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		//for (i = 0, i < quiz.length(), i++){
+		  res.render('quizes/show',{ quiz: quiz})
+		  //res.render('quizes/question',{ pregunta: '¿Italia?'})
+		//}
+	})	
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req,res){
-  var resultado = 'Incorrecta';
   models.Quiz.find(req.params.quizId).then(function(quiz){
-	if (req.query.respuesta === req.quiz.respuesta){
-		resultado = 'Correcta';
+	if (req.query.respuesta === quiz.respuesta){
+	  //if (req.query.respuesta === 'Roma'){
+		res.render('quizes/answer',{respuesta: 'Correcta'});
 	} else{
-		res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado});
+		res.render('quizes/answer',{respuesta: 'Incorrecta'});
 	}
   })
 };
--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-// Autoload de comandos con :quizId
-router.param('quizId', quizController.load); //autoload :quizId
 
-  router.get('/quizes', quizController.index);
-  router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-  router.get('/quizes/:quizId(\\d+)', quizController.show);
+    router.get('/quizes', quizController.index);
+	router.get('/quizes/:quizId(\\d+)', quizController.show);
+    router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+ // router.get('/quizes/answer', quizController.answer);
+ // router.get('/quizes/question', quizController.question);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
 });
-
 
 module.exports = router;
 //
