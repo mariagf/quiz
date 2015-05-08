dd8da9bf6e053831ef56d0a971c3b76a79087400
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-08 12:32:13 +0200

    Quiz 9

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,30 @@
 var models = require('../models/models.js');
 
+// GET /quizes
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/show',{quizes: quizes});
+	})
+};
+
 // GET /quizes/question
 exports.question = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question',{pregunta: '¿Cuál es la capital de Italia?'})
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz: quiz});
 	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta: 'Correcta'});
+	models.Quiz.find(rew.params.quizId).then(function(quiz){
+		if (req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer',{
+							quiz: quiz,
+							respuesta: 'Correcta'});
 		} else{
-			res.render('quizes/answer',{respuesta: 'Incorrecta'});
+			res.render('quizes/answer',{
+							quiz: quiz,
+							respuesta: 'Incorrecta'});
 		}
 	})
 	
