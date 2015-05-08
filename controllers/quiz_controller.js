80cf6c977a161c2ecf2f382fd21651c9bba439a0
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-08 19:14:26 +0200

    vv

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,14 +3,14 @@ var models = require('../models/models.js');
 // GET /quizes
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('/quizes/index.ejs',{quizes: quizes});
+		res.render('quizes/index',{quizes: quizes});
 	})
 };
 
 // GET /quizes/:id
 exports.show = function(req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('/quizes/show',{quiz: quiz});
+		res.render('quizes/show',{quiz: quiz});
 	})
 };
 
@@ -18,11 +18,11 @@ exports.show = function(req,res){
 exports.answer = function(req,res){
 	models.Quiz.find(rew.params.quizId).then(function(quiz){
 		if (req.query.respuesta === quiz.respuesta){
-			res.render('/quizes/answer',{
+			res.render('quizes/answer',{
 							quiz: quiz,
 							respuesta: 'Correcta'});
 		} else{
-			res.render('/quizes/answer',{
+			res.render('quizes/answer',{
 							quiz: quiz,
 							respuesta: 'Incorrecta'});
 		}
