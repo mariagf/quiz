dc63c5b19d2667459558544e3980f553b51538eb
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-10 19:07:20 +0200

    3.0

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,7 +17,7 @@ exports.show = function(req,res){
 exports.lista = function(req,res){
 	var x = req.query.busqueda;
 	var y = x.replace("\s", "%");
-	models.Quiz.sort().findAll({where: ["pregunta like ?", '%' + y + '%']}).then(function(quizes){
+	models.Quiz.findAll({where: ["pregunta like ?", '%' + y + '%']}).then(function(quizes){
 		
 		res.render('quizes/lista',{quizes: quizes});
 	})
