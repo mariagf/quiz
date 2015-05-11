1f8c3485d899c98680d662a96b15584cc3481941
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-12 00:07:14 +0200

    Q12

--- models/quiz.js
+++ models/quiz.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes){
 	return sequelize.define('Quiz',
 		{ pregunta: {
 			type: DataTypes.STRING,
-			validate: { notEmpty: {msp: "-> Falta Pregunta"}}
+			validate: { notEmpty: {msg: "-> Falta Pregunta"}}
 		},
 		  respuesta: {
 		  	type: DataTypes.STRING,
