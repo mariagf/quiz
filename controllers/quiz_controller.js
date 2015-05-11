a46ef44450dd48037630b9fe2be46de46c024eae
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-11 21:48:41 +0200

    Quiz 10

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,50 +1,29 @@
 var models = require('../models/models.js');
 
 //Autoload - factoriza el código si ruta incluye :quizId
-exports.load = function(req,res, next, quizId){
-	models.Quiz.find(quizId).then(function(quiz){
-		if (quiz){
-			req.quiz = quiz;
-			next();
-		} else {
-			next(new Error('No existe quizId=' + quizId));
-		}
-	}). catch(function(error){next(error);})
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz) {
+				req.quiz = quiz;
+				next();
+			} else { next(new Error('No existe quizId=' + quizId));}
+		}).catch(function(error) {next(error);});
 };
 
 
 // GET /quizes
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index',{quizes: quizes});
-	}).catch(function (error){next(error);});
-};
-
-// GET /quizes/new
-exports.new = function(req,res){
-	var quiz = models.Quiz.build( // crea objeto quiz
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
-		);
-
-		res.render('quizes/new',{quiz: quiz});
-};
-
-// POST /quizes/create
-exports.create = function(req,res){
-	var quiz = models.Quiz.build( req.body.quiz);
-
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 		res.render('quizes/index', {quizes: quizes});
-	}) // Redirección HTTP (URL relativo) lista de preguntas
+	}).catch(function(error) { next(error);})
 };
-
 
 // GET /quizes/:id
 exports.show = function(req,res){
-	//models.Quiz.find(req.params.quizId).then(function(quiz){
+	
 		res.render('quizes/show',{quiz: req.quiz});
-	//})
+	
 };
 
 exports.lista = function(req,res){
@@ -58,15 +37,15 @@ exports.lista = function(req,res){
 
 // GET /quizes/:id/answer
 exports.answer = function(req,res){
-	//models.Quiz.find(req.params.quizId).then(function(quiz){
-		var resultado = 'Incorrecta';
+	var resultado = 'Incorrecto';
 		if (req.query.respuesta === req.quiz.respuesta){
-			resultado = 'Correcta';
-		}
+				resultado = 'Correcta'});
+		} else{
 			res.render('quizes/answer',{
 							quiz: req.quiz,
 							respuesta: resultado});
-	//})
+		}
+	})
 	
 };
 
