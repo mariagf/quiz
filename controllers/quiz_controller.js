4389c45cf462a7f221199e32af27bf99add16242
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-11 23:03:31 +0200

    vv

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,14 +15,14 @@ exports.load = function(req, res, next, quizId){
 // GET /quizes
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
+		res.render('quizes/index', {quizes: quizes, errors: []});
 	}).catch(function(error) { next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req,res){
 	
-		res.render('quizes/show',{quiz: req.quiz});
+		res.render('quizes/show',{quiz: req.quiz, errors: []});
 	
 };
 
@@ -31,7 +31,7 @@ exports.new = function(req,res){
 	var quiz = models.Quiz.build( // crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-		res.render('quizes/new',{quiz: quiz});
+		res.render('quizes/new',{quiz: quiz, errors: []});
 };
 
 exports.lista = function(req,res){
@@ -43,14 +43,23 @@ exports.lista = function(req,res){
 	})
 };
 
-// Definción de rutas de /quizes
+// POST /quizes/create
 exports.create = function(req,res){
 	var quiz = models.Quiz.build( req.body.quiz);
-	// guarda en DB los campos pregunta y respuesta de quiz
+	
+	quiz.validate().then(function(err){
+
+	if(err){
+		res.render('quizes/new', {quiz:quiz, errors: err.errors});
+
+	} else{ // guarda en DB los campos pregunta y respuesta de quiz
 	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 
-		res.redirect('/quizes');
-	}) // Redirección HTTP (URL relativo) lista de preguntas
+		res.redirect('/quizes')
+	}) // res.redirect: Redirección HTTP a lista de preguntas
+	}
+   }
+  );
 };
 
 // GET /quizes/:id/answer
@@ -60,6 +69,7 @@ exports.answer = function(req,res){
 		resultado = 'Correcta';
 	} res.render('quizes/answer',{
 							quiz: req.quiz,
-							respuesta: resultado});	
+							respuesta: resultado,
+							errors: [] });	
 };
 
