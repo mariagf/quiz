75fe1765ba5042ae7e5d8ff1876a71c97c82aae6
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-12 17:04:52 +0200

    q16

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -22,7 +22,7 @@ exports.create = function(req,res){
 	} // Crear req.session.user y guardar campos id y username
 	  // La sesión se define por la existencia de: req.session.user
 	  	req.session.user = {id: user.id, username: user.username};
-
+	  	alert(req.session.redir);
 		res.redirect(req.session.redir.toString()); // redirección a path anterior a login
 	
    });
