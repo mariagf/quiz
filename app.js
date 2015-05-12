6ce9242acbfce049cb3e4c861be23cfa52e50bb1
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-12 16:52:20 +0200

    q16

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+//app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser('Quiz 2015'));
 app.use(session());
 app.use(methodOverride('_method'));
