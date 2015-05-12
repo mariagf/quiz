3eac0dfc60d8802bf72060fbd7de35d1de90808d
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-12 20:34:31 +0200

    v

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
-var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 
@@ -24,9 +23,9 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -48,8 +47,7 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err,
-            errors: []
+            error: err
         });
     });
 }
@@ -60,8 +58,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {},
-        errors: []
+        error: {}
     });
 });
 
--- models/quiz.js
+++ models/quiz.js
@@ -1,14 +1,8 @@
-// Definición del modelo de Quiz con validación
+// Definición del modelo de Quiz
 
 module.exports = function(sequelize, DataTypes){
 	return sequelize.define('Quiz',
-		{ pregunta: {
-			type: DataTypes.STRING,
-			validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-		},
-		  respuesta: {
-		  	type: DataTypes.STRING,
-		  	validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-		}
-	});
+		{ pregunta: DataTypes.STRING,
+		  respuesta: DataTypes.STRING,
+		});
 }
