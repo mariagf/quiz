a47c0ecc72ee51a3670f17a22ffd85c5fcc299d4
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-12 10:46:05 +0200

    Q15

---
+++ controllers/comment_controller.js
@@ -0,0 +1,26 @@
+var models = require('../models/models.js');
+
+// GET /quizes/:quizId/comments/new
+exports.new = function(req,res){
+		res.render('comments/new',{quizid: req.params.quizId, errors: []});
+};
+
+// POST /quizes/:quizId/comments
+exports.create = function(req,res){
+	var comment = models.Comment.build({texto: req.body.comment.texto,
+										QuizId: req.params.quizId});
+	
+	comment.validate().then(function(err){
+
+	if(err){
+		res.render('comments/new', {comment:comment, errors: err.errors});
+
+	} else{ // guarda en DB los campos pregunta y respuesta de quiz
+	comment.save().then(function(){
+
+		res.redirect('/quizes/'+req.params.quizId)
+	}) // res.redirect: Redirección HTTP a lista de preguntas
+	}
+   }
+  ).catch(function(error){next(error)});
+};
