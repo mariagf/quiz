0e550dce04a08a9578ca6f2b447962784366729c
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-16 00:09:58 +0200

    q18

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', quizController.load); //autoload :commentId
 
 // Definición de rutas de sesion
   router.get('/login', sessionController.new); // formulario login
@@ -33,6 +34,7 @@ router.param('quizId', quizController.load); //autoload :quizId
 // Definición de rutas de comentarios.
   router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
   router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+  router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
