1467e2dab014cc070eba3213d021df018c54a565
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-15 22:31:51 +0200

    vv

--- app.js
+++ app.js
@@ -11,12 +11,9 @@ var session = require('express-session');
 
 var routes = require('./routes/index');
 
-var pg = require('pg');
-
 //var users = require('./routes/users');
 
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,8 +25,8 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded());
+//app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser('Quiz 2015'));
 app.use(session());
 app.use(methodOverride('_method'));
