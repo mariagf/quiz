692778858c1a0ca7f6eab2da03203d1e5599c647
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-16 00:35:33 +0200

    vv

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.param('commentId', commentController.load); //autoload :commentId
 // Definición de rutas de comentarios.
   router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
   router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-  router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+  router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
