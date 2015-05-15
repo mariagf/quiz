f5098ab7d1048ad57cc9b165ad90dfb2a23ba994
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-15 10:34:32 +0200

    vv

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,41 +1,23 @@
 var models = require('../models/models.js');
 
 //Autoload - factoriza el código si ruta incluye :quizId
-exports.load = function(req, res, next, quizId){
-	models.Quiz.find({ where: {id: Number(quizId)},
-						include:[{model: models.Comment}]
-		}).then(
-		function(quiz){
-			if(quiz) {
-				req.quiz = quiz;
-				next();
-			} else { next(new Error('No existe quizId=' + quizId));}
-		}).catch(function(error) {next(error);});
+exports.load = function(req,res, next, quizId){
+	models.Quiz.find(quizId).then(function(quiz){
+		if (quiz){
+			req.quiz = quiz;
+			next();
+		} else {
+			next(new Error('No existe quizId=' + quizId));
+		}
+	}). catch(function(error){next(error);})
 };
+
 
 // GET /quizes
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes, errors: []});
-	}).catch(function(error) { next(error)});
-};
-
-// GET /quizes/:id
-exports.show = function(req,res){
-		res.render('quizes/show',{quiz: req.quiz, errors: []});
-};
-
-// GET /quizes/:id/edit
-exports.edit = function(req,res){
-		var quiz = req.quiz; //autoload de instancia de quiz
-		res.render('quizes/edit',{quiz: quiz, errors: []});
-};
-
-// DELETE /quizes/:id
-exports.destroy = function(req,res){
-		req.quiz.destroy().then(function(){
-		res.redirect('/quizes');
-	}).catch(function(error) { next(error)});
+		res.render('quizes/index',{quizes: quizes});
+	}).catch(function (error){next(error);});
 };
 
 // GET /quizes/new
@@ -43,7 +25,25 @@ exports.new = function(req,res){
 	var quiz = models.Quiz.build( // crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-		res.render('quizes/new',{quiz: quiz, errors: []});
+		res.render('quizes/new',{quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build( req.body.quiz);
+
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección HTTP (URL relativo) lista de preguntas
+};
+
+
+// GET /quizes/:id
+exports.show = function(req,res){
+	//models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz: req.quiz});
+	//})
 };
 
 exports.lista = function(req,res){
@@ -55,53 +55,17 @@ exports.lista = function(req,res){
 	})
 };
 
-// POST /quizes/create
-exports.create = function(req,res){
-	var quiz = models.Quiz.build( req.body.quiz);
-	
-	quiz.validate().then(function(err){
-
-	if(err){
-		res.render('quizes/new', {quiz:quiz, errors: err.errors});
-
-	} else{ // guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-
-		res.redirect('/quizes')
-	}) // res.redirect: Redirección HTTP a lista de preguntas
-	}
-   }
-  );
-};
-
-// PUT /quizes/:id
-exports.update = function(req,res){
-	req.quiz.pregunta = req.body.quiz.pregunta;
-	req.quiz.respuesta = req.body.quiz.respuesta;
-	
-	req.quiz.validate().then(function(err){
-
-	if(err){
-		res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
-
-	} else{ // guarda en DB los campos pregunta y respuesta de quiz
-	req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-
-		res.redirect('/quizes');
-	}); // res.redirect: Redirección HTTP a lista de preguntas
-	}
-   }
-  );
-};
-
 // GET /quizes/:id/answer
 exports.answer = function(req,res){
-	var resultado = 'Incorrecta';
-	if (req.query.respuesta === req.quiz.respuesta){
-		resultado = 'Correcta';
-	} res.render('quizes/answer',{
+	//models.Quiz.find(req.params.quizId).then(function(quiz){
+		var resultado = 'Incorrecta';
+		if (req.query.respuesta === req.quiz.respuesta){
+			resultado = 'Correcta';
+		}
+			res.render('quizes/answer',{
 							quiz: req.quiz,
-							respuesta: resultado,
-							errors: [] });	
+							respuesta: resultado});
+	//})
+	
 };
 
