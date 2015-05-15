c9cdf42e2636992e65d8a44e6e3762c822722159
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-15 14:00:35 +0200

    vv

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
-var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
-var pg = require('pg');
+
 //var users = require('./routes/users');
 
 var app = express();
@@ -24,8 +23,8 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
