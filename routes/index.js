9f058d615b44282b1790a9afc8f9008a3c5b77dd
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-15 12:24:21 +0200

    vv

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de /quizes
+  router.get('/quizes', quizController.index);
+  router.get('/quizes/:quizId(\\d+)', quizController.show);
+  router.get('/quizes/new', quizController.new);
+  router.get('/lista', quizController.lista);
+  router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);  router.get('/lista', quizController.lista);
+  router.post('/quizes/create', quizController.create);
+  router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+  router.put('/quizes/:quizId(\\d+)', quizController.update);
+  router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
+module.exports = router;
