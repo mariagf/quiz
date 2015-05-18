ccce00b08d2ea6ee03446802ba06933655ae3907
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-18 13:08:21 +0200

    vv

--- routes/index.js
+++ routes/index.js
@@ -23,20 +23,20 @@ router.param('commentId', commentController.load); //autoload :commentId
  
 
 // Definición de rutas de /quizes
-  router.get('/quizes', quizController.index);
-  router.get('/quizes/:quizId(\\d+)', quizController.show);
-  router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-  router.get('/lista', quizController.lista);
-  router.get('/quizes/new', sessionController.loginRequired, quizController.new);
-  router.post('/quizes/create', sessionController.loginRequired, quizController.create);
-  router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
-  router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
-  router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
+  router.get('/quizes', sessionController.timeout, quizController.index);
+  router.get('/quizes/:quizId(\\d+)', sessionController.timeout, quizController.show);
+  router.get('/quizes/:quizId(\\d+)/answer', sessionController.timeout, quizController.answer);
+  router.get('/lista', sessionController.timeout, quizController.lista);
+  router.get('/quizes/new', sessionController.timeout, sessionController.loginRequired, quizController.new);
+  router.post('/quizes/create', sessionController.timeout, sessionController.loginRequired, quizController.create);
+  router.get('/quizes/:quizId(\\d+)/edit', sessionController.timeout, sessionController.loginRequired, quizController.edit);
+  router.put('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.update);
+  router.delete('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios.
   router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
   router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-  router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+  router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.timeout, sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
