c7bff34f87e17e7582d49efa3d9df255668bc9e1
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-18 11:59:10 +0200

    vv

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -51,7 +51,7 @@ exports.new = function(req,res){
 exports.statistics = function(req,res){
 	models.Comment.findAll().then(function(comment){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/statistics',{quiz: req.quiz, quizes: quizes, comment: comment, errors: []});
+		res.render('quizes/statistics',{quizes: quizes, comment: comment, errors: []});
 	})
 	}).catch(function(error) { next(error)});
 };
