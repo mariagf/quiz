40fc1a1996b7e1933b936af1404e1b9fa8e07586
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-18 14:26:35 +0200

    vv

--- models/timeout.js
+++
@@ -1,8 +0,0 @@
-// Mensaje alerta
-
-module.exports = function(sequelize, DataTypes){
-	return sequelize.define('Quiz',
-		validate: {{msg: "¡Su sesión ha expirado!\n\nIntroduzca de nuevo su usuario y contraseña por favor."}}
-		}
-	});
-}
