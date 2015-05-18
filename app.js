9209dca5a27bee0fb2b71ea74408348d50d5815f
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-18 14:30:17 +0200

    vv

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(partials());
-
+app.use(flash());
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
