1061c8322527fcf31d53a6f1f457d2a532aaabe1
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-19 23:04:05 +0200

    vv

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,7 +9,7 @@ exports.autenticar = function(login, password, callback){
 		}
 	}). then(function(user){
 		if(user){
-			if (user.verifyPassword(password){
+			if (user.verifyPassword(password)){
 				callback(null, user);
 			} else { callback(new Error('Password erróneo.')); }
 		} else { callback(new Error('No existe user = ' + login))}
