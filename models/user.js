7ffa3ec65e0ddd6a33b65787defa3c2ce15c457c
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-19 20:41:37 +0200

    vv

--- models/user.js
+++ models/user.js
@@ -3,14 +3,14 @@ var crypto = require('crypto');
 var key = process.env.PASSWORD_ENCRYPTION_KEY;
 
 module.exports = function(sequelize, DataTypes){
-	var User = sequelize.define('User',
-		{ username: {
+	var User = sequelize.define('User',{ 
+
+		username: {
 			type: DataTypes.STRING,
 			unique: true,
-			validate: { 
-				notEmpty: {msg: "-> Falta username"}},
-				// -> devuelve mensaje de error si username ya existe
-				isUnique: function (value, next){
+			validate: { notEmpty: {msg: "-> Falta username"}},
+			// -> devuelve mensaje de error si username ya existe
+			isUnique: function (value, next){
 					var self = this;
 					User.find({where: {username: value}})
 					.then(function (user) {
@@ -19,7 +19,6 @@ module.exports = function(sequelize, DataTypes){
 						}
 						return next();
 					}).catch(function (err) {return next(err);});
-				}
 			}
 		},
 		  password: {
