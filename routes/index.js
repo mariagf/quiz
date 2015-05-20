44a57f1c11039f39b84d357b2eb32fbb061322be
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-20 23:50:20 +0200

    vv

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var multer = requiere('multer');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
@@ -37,10 +36,8 @@ router.param('userId', userController.load); //autoload :userId
   router.get('/quizes/:quizId(\\d+)/answer', sessionController.timeout, quizController.answer);
   router.get('/lista', sessionController.timeout, quizController.lista);
   router.get('/quizes/new', sessionController.timeout, sessionController.loginRequired, quizController.new);
-  //router.post('/quizes/create', sessionController.timeout, sessionController.loginRequired, multer( dest: './public/media'{}), quizController.create);
   router.post('/quizes/create', sessionController.timeout, sessionController.loginRequired, quizController.create);
   router.get('/quizes/:quizId(\\d+)/edit', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
-  //router.put('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, multer( dest: './public/media'{}), quizController.update);
   router.put('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, quizController.update);
   router.delete('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
 
