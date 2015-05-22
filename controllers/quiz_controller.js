adbc9f940314ef5c4fde97c5e7cf6d6ce0361b49
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-22 20:33:34 +0200

    Q24

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -108,7 +108,7 @@ exports.create = function(req,res){
 		res.render('quizes/new', {quiz:quiz, errors: err.errors});
 
 	} else{ // guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta", "UserId", "image"]}).then(function(){
+	quiz.save({fields: ["pregunta", "respuesta", "UserId"]}).then(function(){
 
 		res.redirect('/quizes')
 	}) // res.redirect: Redirección HTTP a lista de preguntas
