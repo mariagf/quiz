e1d96997b48b8c7e1058e14154ba5a810842a154
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-22 19:32:01 +0200

    vv

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -30,8 +30,14 @@ exports.load = function(req, res, next, quizId){
 };
 
 // GET /quizes
+// GET /users/.userId/quizes
 exports.index = function(req,res){
-	models.Quiz.findAll().then(function(quizes){
+	var options = {};
+	if(req.user){
+		options.where = {UserId: req.user.id}
+	}
+
+	models.Quiz.findAll(options).then(function(quizes){
 		res.render('quizes/index', {quizes: quizes, errors: []});
 	}).catch(function(error) { next(error)});
 };
@@ -88,6 +94,11 @@ exports.lista = function(req,res){
 // POST /quizes/create
 exports.create = function(req,res){
 	req.body.quiz.UserId = req.session.user.id;
+	
+	if(req.files.image){
+		req.body.quiz.image = req.files.image.name;
+	}
+
 	var quiz = models.Quiz.build( req.body.quiz );
 	
 	quiz.validate().then(function(err){
@@ -107,6 +118,10 @@ exports.create = function(req,res){
 
 // PUT /quizes/:id
 exports.update = function(req,res){
+	if(req.files.image){
+		req.quiz.image = req.files.image.name;
+	}
+
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
 	
@@ -116,7 +131,7 @@ exports.update = function(req,res){
 		res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 
 	} else{ // guarda en DB los campos pregunta y respuesta de quiz
-	req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+	req.quiz.save({fields: ["pregunta", "respuesta", "image"]}).then(function(){
 
 		res.redirect('/quizes');
 	}); // res.redirect: Redirección HTTP a lista de preguntas
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
@@ -29,17 +30,18 @@ router.param('userId', userController.load); //autoload :userId
   router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userController.ownershipRequired, userController.edit);
   router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.update);
   router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.destroy);
+  router.get('/user/:userId(\\d+)/quizes', quizController.index);
 
 // Definición de rutas de /quizes
   router.get('/quizes', sessionController.timeout, quizController.index);
   router.get('/quizes/:quizId(\\d+)', sessionController.timeout, quizController.show);
   router.get('/quizes/:quizId(\\d+)/answer', sessionController.timeout, quizController.answer);
   router.get('/lista', sessionController.timeout, quizController.lista);
-  router.get('/quizes/new', sessionController.timeout, sessionController.loginRequired, quizController.new);
+  router.get('/quizes/new', sessionController.timeout, sessionController.loginRequired, multer({ dest: './public/media/'}), quizController.new);
   router.post('/quizes/create', sessionController.timeout, sessionController.loginRequired, quizController.create);
   router.get('/quizes/:quizId(\\d+)/edit', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
   router.put('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, quizController.update);
-  router.delete('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
+  router.delete('/quizes/:quizId(\\d+)', sessionController.timeout, sessionController.loginRequired, quizController.ownershipRequired, multer({ dest: './public/media/'}), quizController.destroy);
 
 // Definición de rutas de comentarios.
   router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
